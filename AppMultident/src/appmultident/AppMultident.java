5080d34ec0dbd3c83f6a112e576f476088b207e3
Juan Lozada <juanlozadacochaches@gmail.com>
2024-12-04 21:06:32 -0500

    update

--- AppMultident/src/appmultident/AppMultident.java
+++ AppMultident/src/appmultident/AppMultident.java
@@ -6,11 +6,13 @@ package appmultident;
 
 import Controlador.ControladorCitas;
 import Controlador.ControladorHistorialClinico;
+import Controlador.ControladorLogin;
+import Modelo.LoginModel;
 import Vista.VistaGestionCitas;
 import Vista.VistaHistorialClinicoPaciente;
-//import com.formdev.flatlaf.themes.FlatMacLightLaf;
-//import javax.swing.UIManager;
-
+import Vista.VistaLogin;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import javax.swing.UIManager; 
 /**
  
  * @author LOZADA
@@ -21,17 +23,16 @@ public class AppMultident {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //FlatMacLightLaf.setup();
-        //UIManager.put("Button.arc", 25); 
-        /*
-        VistaGestionCitas vg1 = new VistaGestionCitas();
-        ControladorCitas c1 = new ControladorCitas(vg1);
-        */
+        FlatMacLightLaf.setup();
+        UIManager.put("Button.arc", 20); 
+        LoginModel loginModel = new LoginModel();
+        VistaLogin vistaLogin = new VistaLogin();
+
+        // Crear el controlador, el cual manejará la vista y el modelo
+        ControladorLogin loginController = new ControladorLogin(vistaLogin, loginModel);
         
-      
-        
-        VistaHistorialClinicoPaciente vhc1 = new VistaHistorialClinicoPaciente();
-        ControladorHistorialClinico chc1= new ControladorHistorialClinico(vhc1);
+        vistaLogin.setLocationRelativeTo(null);
+        vistaLogin.setVisible(true);  // Mostrar la vista de login
         
     }
     
